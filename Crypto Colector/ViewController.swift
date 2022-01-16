4eff20fd4dec82b7c80a7e5c0812ba59dc1e411f
Paweł Pabisiak <pawel.pabisiak@onet.eu>
2022-01-16 21:37:23 +0100

    Initial Commit

---
+++ Crypto Colector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Crypto Colector
+//
+//  Created by Paweł on 16/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
