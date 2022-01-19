700f28dc3bc728059b6b788a9b1fe459662b3463
Paweł Pabisiak <pawel.pabisiak@onet.eu>
2022-01-19 23:41:45 +0100

    Data got from internet and decode as a string

--- Crypto Colector/Model/PriceManager.swift
+++ Crypto Colector/Model/PriceManager.swift
@@ -13,9 +13,33 @@ struct PriceManager {
     
     func fetchCoinPrice (coinName: String, fiatName: String) {
         let urlString = "\(priceURL)\(coinName)/\(fiatName)?apikey=11161ACB-0926-4667-B7B3-82277B4164B6"
-        print(urlString)
+        performRequest(urlString: urlString)
     }
     
-
+    func performRequest(urlString: String) {
+        // Step 1 tworzę url:
+        if let url = URL(string: urlString) {
+            // Step 2 tworzę sesję
+            let session = URLSession(configuration: .default)
+            // Step 3 przypisuję sesję do zadania:
+            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            
+            // Step 4 start Task
+            task.resume()
+        }
+        
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
+    }
     
 }
